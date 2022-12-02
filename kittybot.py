f58120714c51dae26a15340e6e8fda2bb8913e9f
Марина Слобожанинова <slobozhaninovaw@gmail.com>
2022-12-02 23:23:45 +0300

    добавлены кнопки

--- kittybot.py
+++ kittybot.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
-from telegram import Bot
+from telegram import Bot, ReplyKeyboardMarkup
 
 token = '5841478407:AAFFAAILJqbLjPH_gDAJqUmyh34EENdIPkk'
 updater = Updater(token=token)
@@ -8,17 +8,25 @@ bot = Bot(token=token)
 
 def say_hi(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='hiii')
+    context.bot.send_message(chat_id=chat.id, text='hiii, \U0001f525')
 
 
 def wake_up(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='hello')
+    name = update.message.chat.first_name
+    buttons = ReplyKeyboardMarkup(
+        [['Тык', 'айпи'],
+         ['который час']
+         ],
+        one_time_keyboard=True, )
+    context.bot.send_message(chat_id=chat.id,
+                             text=f'hello, {name}',
+                             reply_markup=buttons)
 
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
 
-updater.start_polling(poll_interval=20.2)
+updater.start_polling()
 
 updater.idle()
